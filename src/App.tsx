8af69c05e7f4790d3273f2e2a50fe6fb8e077a85
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-03-29 19:40:44 -0300

    Exibição do componente header e configurações iniciais das rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import { Home } from "./pages/Home";
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
-  return <div className="App">Projeto strONG</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </BrowserRouter>
+
+      <GlobalStyle />
+    </>
+  );
 }
