9c0aaccbf65202cc2c78c8e79bb6c36ee8a7363a
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-04-01 19:46:07 -0300

    página q será exibida o componente de feedback

---
+++ src/pages/Feedbacks.tsx
@@ -0,0 +1,13 @@
+import { Feedback } from "../components/Feedback";
+import { Footer } from "../components/Footer";
+import { Header } from "../components/Header";
+
+export function Feedbacks() {
+  return (
+    <>
+      <Header />
+      <Feedback />
+      <Footer />
+    </>
+  );
+}
