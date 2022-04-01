f09753a6f5e4175126e4b80bab4b2b02c357b168
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-04-01 19:47:33 -0300

    adiçãõ da rota do feedback

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Feedbacks } from "./pages/Feedbacks";
 
 import { Home } from "./pages/Home";
 import { GlobalStyle } from "./styles/global";
@@ -6,13 +7,14 @@ import { GlobalStyle } from "./styles/global";
 export function App() {
   const currentPage = window.location.pathname;
 
-  currentPage == "/" && (window.location.href = "/home");
+  currentPage === "/" && (window.location.href = "/home");
 
   return (
     <>
       <BrowserRouter>
         <Switch>
           <Route path="/home" exact component={Home} />
+          <Route path="/feedbacks" exact component={Feedbacks} />
         </Switch>
       </BrowserRouter>
 
