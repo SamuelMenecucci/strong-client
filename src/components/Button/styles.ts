dfff3135beee5b0353bf025a8165f1b288f66648
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-04-05 11:01:32 -0300

    remoção de estilizações globais para possibiliadde de alteração em outros componentes

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -3,8 +3,6 @@ import styled from "styled-components";
 export const ButtonComponent = styled.button`
   background: linear-gradient(to right, #b224ef, #7579ff, #7579ff);
 
-  width: 318px;
-  padding: 16px 50px;
   border-radius: 16px;
 
   margin-top: 50px;
