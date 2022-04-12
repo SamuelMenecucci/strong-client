f3699a3c8893927178887e2154f29501535b1aa8
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-04-12 17:22:39 -0300

    adicionado tipagem da ong

--- src/shared/models.ts
+++ src/shared/models.ts
@@ -2,3 +2,12 @@ export type ModalProps = {
   isOpen: boolean;
   onRequestClose: () => void;
 };
+
+export type OngType = {
+  nomeOng: string;
+  email: string;
+  tel: string;
+  cnpj: string;
+  description: string;
+  senha: string;
+};
