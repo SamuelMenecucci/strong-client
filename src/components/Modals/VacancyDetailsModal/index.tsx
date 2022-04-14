b9dd9b7a08c5cd2a83d52da30c6aa423494fd529
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-04-14 12:51:14 -0300

    criação e estrutura inicial

--- src/components/Modals/VacancyDetailsModal/index.tsx
+++ src/components/Modals/VacancyDetailsModal/index.tsx
@@ -1,6 +1,25 @@
 import Modal from "react-modal";
 import { ModalProps } from "../../../shared/models";
+import { Container } from "./styles";
+import closeImg from "../../../assets/close.svg";
+import { TextareaInput } from "../../Inputs/Textarea/styles";
 
 export function VacancyDetailsModal({ isOpen, onRequestClose }: ModalProps) {
-  return <Modal isOpen={isOpen} onRequestClose={onRequestClose}></Modal>;
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      className="react-VacancyDetailsModal"
+      overlayClassName="react-modal-overlay"
+    >
+      <Container>
+        <button className="closeButton" onClick={onRequestClose}>
+          <img src={closeImg} alt="" />
+        </button>
+        <h1>Titulo vaga</h1>
+        HTML - CSS - Javascript
+        <TextareaInput />
+      </Container>
+    </Modal>
+  );
 }
