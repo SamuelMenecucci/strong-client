6622e253f160f0f1cc37e770846e06a2ac0b6047
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-04-14 14:23:29 -0300

    configurações iniciais

--- src/components/Modals/VacancyDetailsModal/index.tsx
+++ src/components/Modals/VacancyDetailsModal/index.tsx
@@ -3,6 +3,8 @@ import { ModalProps } from "../../../shared/models";
 import { Container } from "./styles";
 import closeImg from "../../../assets/close.svg";
 import { TextareaInput } from "../../Inputs/Textarea/styles";
+import { CloseModalButton } from "../../Buttons/CloseModalButton";
+import { TelInput } from "../../Inputs/TelInput";
 
 export function VacancyDetailsModal({ isOpen, onRequestClose }: ModalProps) {
   return (
@@ -13,13 +15,15 @@ export function VacancyDetailsModal({ isOpen, onRequestClose }: ModalProps) {
       overlayClassName="react-modal-overlay"
     >
       <Container>
-        <button className="closeButton" onClick={onRequestClose}>
-          <img src={closeImg} alt="" />
-        </button>
+        <CloseModalButton onClick={onRequestClose} />
         <h1>Titulo vaga</h1>
         HTML - CSS - Javascript
-        <TextareaInput />
+        <TelInput />
+        <TextareaInput placeholder="" value={text} />
       </Container>
     </Modal>
   );
 }
+
+const text =
+  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the ";
