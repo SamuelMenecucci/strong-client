2b96c1b81da4d5303d78e40acaf9e0c2a9c581e0
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-04-15 18:17:31 -0300

    correção do componente de cnpj para aceitar os atributos passados no componente de registro

--- src/components/Inputs/CNPJInput/index.tsx
+++ src/components/Inputs/CNPJInput/index.tsx
@@ -3,6 +3,11 @@ import InputMask from "react-input-mask";
 
 export function CNPJInput({ ...props }: InputHTMLAttributes<HTMLInputElement>) {
   return (
-    <InputMask mask="99.999.999./9999-99" type="text" placeholder="CNPJ" />
+    <InputMask
+      mask="99.999.999./9999-99"
+      type="text"
+      placeholder="CNPJ"
+      {...props}
+    />
   );
 }
