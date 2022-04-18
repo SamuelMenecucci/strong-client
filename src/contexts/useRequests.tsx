92d54ee9a40cee479e66400ddbc26115a11eb687
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-04-18 09:43:00 -0300

    correção da gravação do retorno da api no sessionStorage

--- src/contexts/useRequests.tsx
+++ src/contexts/useRequests.tsx
@@ -15,22 +15,33 @@ export function RequestsProvider({ children }: any) {
     return "";
   });
 
+  console.log(loggedOng);
+
   const [feedbacks, setFeedbacks] = useState<any>([]);
 
   const [vagas, setVagas] = useState([]);
 
   async function createOng(data: OngType) {
-    api
-      .post("createOng", { ...data })
-      .then((res) => {
-        sessionStorage.setItem("ong", JSON.stringify(res.data));
-      })
-      .catch((err) => err);
+    //TODO descobrir o pq dessa forma não funciona. não grava os dados do retorno no sessionStorage
+    // api
+    //   .post("createOng", { ...data })
+    //   .then((res) => {
+    //     sessionStorage.setItem("ong", JSON.stringify(res.data));
+    //   })
+    //   .catch((err) => err);
+
+    const result = await api.post("createOng", { ...data });
+
+    sessionStorage.setItem("ong", JSON.stringify(result.data));
+  }
+
+  function editOng(data: any) {
+    api.put("editOng", { ...data });
   }
 
   return (
     <RequestsContext.Provider
-      value={{ feedbacks, createOng, vagas, loggedOng }}
+      value={{ feedbacks, createOng, vagas, loggedOng, setLoggedOng }}
     >
       {children}
     </RequestsContext.Provider>
