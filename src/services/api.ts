5ec8141e323899925c5895777fd53a24841e9e46
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-04-22 17:36:48 -0300

    alteração da rota para o json-server. criação da nova rota

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,3 +3,7 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: "http://localhost:5001",
 });
+
+export const jsonServer = axios.create({
+  baseURL: "http://localhost:5050",
+});
