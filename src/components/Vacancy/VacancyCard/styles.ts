5f9e4c96da36238846b5050a7ac567d55f123947
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-04-27 19:26:21 -0300

    alteração da estilização pela proprieadade passada, refletindo os diferentes tamanhos que o componente irá ter

--- src/components/Vacancy/VacancyCard/styles.ts
+++ src/components/Vacancy/VacancyCard/styles.ts
@@ -1,27 +1,33 @@
 import styled from "styled-components";
 
-export const Content = styled.div`
-  width: 375px;
-  height: 190px;
+interface ContentProps {
+  sizeType: string;
+}
+
+export const Content = styled.div<ContentProps>`
+  width: ${({ sizeType }) => (sizeType === "profileModal" ? "246px" : "375px")};
+  height: ${({ sizeType }) =>
+    sizeType === "profileModal" ? "125px" : "190px"};
   background: linear-gradient(
     to right,
-    rgba(178, 36, 239, 0.3),
-    rgba(117, 121, 255, 0.3),
-    rgba(117, 121, 255, 0.3)
+    rgba(178, 36, 239, 0.1),
+    rgba(117, 121, 255, 0.1),
+    rgba(117, 121, 255, 0.1)
   );
   border-radius: 40px;
 
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: left;
 
   padding: 20px 20px;
 
   cursor: pointer;
 
   span {
-    width: 109px;
-    height: 109px;
+    width: ${(props) => (props.sizeType === "profileModal" ? "70px" : "109px")};
+    height: ${(props) =>
+      props.sizeType === "profileModal" ? "80px" : "109px"};
     margin-right: 14px;
 
     img {
@@ -34,7 +40,8 @@ export const Content = styled.div`
 
   .info {
     h1 {
-      font-size: 24px;
+      font-size: ${({ sizeType }) =>
+        sizeType === "profileModal" ? "16px" : "24px"};
       background: -webkit-linear-gradient(
         var(--purple-color),
         var(--blue-color)
@@ -50,12 +57,15 @@ export const Content = styled.div`
     }
 
     p {
-      font-size: 14px;
+      font-size: ${({ sizeType }) =>
+        sizeType === "profileModal" ? "9px" : "14px"};
     }
   }
 `;
 
-export const Cards = styled.div`
+export const Cards = styled.div<ContentProps>`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
+  gap: ${({ sizeType }) =>
+    sizeType === "profileModal" ? "20px 10px" : "50px 50px"};
 `;
