a53b6c095d6012b9f957b6234015e7d68f3eb28c
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-04-27 22:45:44 -0300

    alteração de instruções de busca para o usuário

--- src/components/Inputs/Search/index.tsx
+++ src/components/Inputs/Search/index.tsx
@@ -4,7 +4,11 @@ import { Container } from "./styles";
 export function Search() {
   return (
     <Container>
-      <input type="search" placeholder="Search..." name="filter" />{" "}
+      <input
+        type="search"
+        placeholder="Digite o titulo ou a tag que deseja buscar"
+        name="filter"
+      />
       <button type="submit"></button>
       <img src={searchImg} alt="" />
     </Container>
