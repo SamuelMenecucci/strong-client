32a155b3995659bdca34808a96f1b2a644e47b45
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-04-28 21:42:08 -0300

    criação de mascara para o cnpj

--- src/Helper/index.js
+++ src/Helper/index.js
@@ -15,3 +15,27 @@ export const PhotosUpload = {
     reader.readAsDataURL(fileList[0]);
   },
 };
+
+export const InputMasks = {
+  apply(input, func) {
+    setTimeout(() => {
+      input.value = InputMasks[func](input.value);
+    }, 1);
+  },
+  CNPJMask(value) {
+    value = value.replace(/\D/g, "");
+
+    if (value.length > 14) value = value.slice(0, -1);
+
+    value = value.replace(/(\d{2})(\d)/, "$1.$2");
+    value = value.replace(/(\d{3})(\d)/, "$1.$2");
+    value = value.replace(/(\d{3})(\d)/, "$1/$2");
+    value = value.replace(/(\d{4})(\d)/, "$1-$2");
+
+    return value;
+  },
+
+  TelMask(value) {
+    value = value.replace(/\D/g, "");
+  },
+};
