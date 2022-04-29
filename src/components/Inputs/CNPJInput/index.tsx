916b69035a4a53f6c17952c58343f013edf18522
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-04-28 21:43:31 -0300

    substituição do inputMask por um input que recebe a mascara do helper

--- src/components/Inputs/CNPJInput/index.tsx
+++ src/components/Inputs/CNPJInput/index.tsx
@@ -1,14 +1,5 @@
 import { InputHTMLAttributes } from "react";
-import InputMask from "react-input-mask";
 
 export function CNPJInput({ ...props }: InputHTMLAttributes<HTMLInputElement>) {
-  return (
-    <InputMask
-      id="cnpj"
-      mask="99.999.999./9999-99"
-      type="text"
-      placeholder="CNPJ"
-      {...props}
-    />
-  );
+  return <input typeof="text" id="cnpj" placeholder="CNPJ" {...props} />;
 }
