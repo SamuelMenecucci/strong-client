36a66da09ffa1d7d141747d1c5f6df6d5be4773f
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-05-09 21:38:56 -0300

    estilização dos campos do modal

---
+++ src/components/Modals/LoginModal/styles.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Container = styled.form`
+  width: 100%;
+
+  label {
+    font-size: 24px;
+    color: #fff;
+    font-weight: 600;
+  }
+
+  button[type="submit"] {
+    padding: 11px 60px;
+
+    background: white;
+    border-radius: 16px;
+
+    width: 100%;
+
+    h1 {
+      margin-bottom: initial;
+    }
+  }
+`;
