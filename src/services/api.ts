94ea55a38419c345bc59b371ac4ac544d7598456
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-05-09 21:38:34 -0300

    configuração do axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,8 +2,5 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: "http://localhost:5001",
-});
-
-export const jsonServer = axios.create({
-  baseURL: "http://localhost:5050",
+  withCredentials: true,
 });
