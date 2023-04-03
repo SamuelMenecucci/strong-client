d4e9e643bd82aec698ecfadbeb1a7ce706066774
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2023-04-03 20:00:21 -0300

    adição de variável ambiente para preservar o endereço do backend

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5001",
+  // baseURL: "http://localhost:5001",
+  baseURL: process.env.REACT_APP_API_ADDRESS,
   withCredentials: true,
 });
